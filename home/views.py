cddf241816e546cac33ebc5762a792a1aa31abe3
Mehedi Hasan <mehedihasan21903@gmail.com>
2024-11-18 05:16:50 +0600

    which category has subcategory this category will show

--- home/views.py
+++ home/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from category.models import *
 from subcat.models import *
-from django.db.models import Prefetch
+from django.db.models import Prefetch, Count
 # Create your views here.
 def homePage(req):
-    all_data_subcat=SubCategories.objects.prefetch_related('cat_id').all()
+    all_cats_subcats = SubCategories.objects.prefetch_related('cat_id').all()
     '''for i in all_data_subcat:
         print(i)'''
-    categories = Categories.objects.prefetch_related(
+    categories = Categories.objects.annotate(subcat_count=Count('Subcategories')).filter(subcat_count__gt=0).prefetch_related(
         Prefetch('Subcategories', queryset=SubCategories.objects.all())
     ).distinct()
     for category in categories:
